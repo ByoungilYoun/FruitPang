ae433757abacac533e8b9d959ae0531225bfb425
ByoungilYoun <jimmy7525@naver.com>
2020-09-29 11:07:37 +0900

    Make getHardCards function in CardModel

--- FruitPang/FruitPang/Model/CardModel.swift
+++ FruitPang/FruitPang/Model/CardModel.swift
@@ -69,4 +69,33 @@ class CardModel {
     return generatedCardsArray
   }
   
+  //MARK: - getHardCards()
+  func getHardCards() -> [Card] {
+    var generatedNumbersArray = [Int]()
+    var generatedCardsArray = [Card]()
+    
+    while generatedNumbersArray.count < 10 {
+      let randomNumber = arc4random_uniform(13) + 1
+      
+      if generatedNumbersArray.contains(Int(randomNumber)) == false {
+        generatedNumbersArray.append(Int(randomNumber))
+        
+        let cardOne = Card()
+        cardOne.imageName = "card\(randomNumber)"
+        generatedCardsArray.append(cardOne)
+        
+        let cardTwo = Card()
+        cardTwo.imageName = "card\(randomNumber)"
+        generatedCardsArray.append(cardTwo)
+      }
+    }
+    
+    for i in 0...generatedCardsArray.count - 1 {
+      let randomNumber = Int(arc4random_uniform(UInt32(generatedCardsArray.count)))
+      let temporaryStorage = generatedCardsArray[i]
+      generatedCardsArray[i] = generatedCardsArray[randomNumber]
+      generatedCardsArray[randomNumber] = temporaryStorage
+    }
+    return generatedCardsArray
+  }
 }
