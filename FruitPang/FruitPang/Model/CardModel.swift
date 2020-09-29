83de320c0ff78ada183bc5a8f2ae115e958c5fcf
ByoungilYoun <jimmy7525@naver.com>
2020-09-29 10:48:23 +0900

    Make getEasyCards function in CardModel

---
+++ FruitPang/FruitPang/Model/CardModel.swift
@@ -0,0 +1,42 @@
+//
+//  CardModel.swift
+//  FruitPang
+//
+//  Created by 윤병일 on 2020/09/29.
+//
+
+import Foundation
+
+class CardModel {
+  
+  //MARK: - getEasyCards()
+  func getEasyCards() -> [Card] {
+    var generatedNumbersArray = [Int]()
+    var generatedCardsArray = [Card]()
+    
+    while generatedNumbersArray.count < 6 {
+      let randomNumber = arc4random_uniform(13) + 1
+      
+      if generatedNumbersArray.contains(Int(randomNumber)) == false {
+        generatedNumbersArray.append(Int(randomNumber))
+        
+        let cardOne = Card()
+        cardOne.imageName = "card\(randomNumber)"
+        generatedCardsArray.append(cardOne)
+        
+        let cardTwo = Card()
+        cardTwo.imageName = "card\(randomNumber)"
+        generatedCardsArray.append(cardTwo)
+      }
+    }
+    
+    for i in 0...generatedCardsArray.count - 1 {
+      let randomNumber = Int(arc4random_uniform(UInt32(generatedCardsArray.count)))
+      let temporaryStorage = generatedCardsArray[i]
+      generatedCardsArray[i] = generatedCardsArray[randomNumber]
+      generatedCardsArray[randomNumber] = temporaryStorage
+    }
+    return generatedCardsArray
+  }
+  
+}
